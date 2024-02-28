64a96374a706a6cb59de3524646184e0acac4cba
Pete Mettaprasert <collegeforpete@gmail.com>
2024-02-28 15:27:31 -0800

    Start of the client application

---
+++ ClientSide/CommandLineClient.py
@@ -0,0 +1,8 @@
+import requests
+
+def get_image_path():
+    return input("Please enter the location of the image you want to modify: ")
+
+def get_operations():
+    print("Here are the available operations: ")
+    print
