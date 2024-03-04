c3197c41ef58c8a5492b3d94bda24a5b94827927
Pete Mettaprasert <collegeforpete@gmail.com>
2024-03-03 20:38:17 -0800

    Refactor image processing imports and add comments

--- OpenCV/ProcessingService.py
+++ OpenCV/ProcessingService.py
@@ -4,9 +4,14 @@ import io
 from imghdr import what
 from datetime import datetime  # For generating unique filenames
 
-# Import your image processing classes
-from .ImageProcessor import FlipProcessor, RotateProcessor, GrayscaleProcessor, ResizeProcessor, ThumbnailProcessor, RotateLeftProcessor, RotateRightProcessor
+# Import the image processing classes
+from .ImageProcessor import FlipProcessor,  \
+RotateProcessor, GrayscaleProcessor, \
+ResizeProcessor, ThumbnailProcessor, \
+RotateLeftProcessor, RotateRightProcessor
 
+
+# Check the flip direction and convert it to the appropriate OpenCV code
 def convert_direction_to_code(direction):
     if direction == 'horizontal':
         return 1
@@ -14,15 +19,20 @@ def convert_direction_to_code(direction):
         return 0
     else:
         raise ValueError("Invalid flip direction. Must be 'horizontal' or 'vertical'.")
-    
+
+# Validate the resize percentage   
 def validate_resize_percentage(percentage):
     if not -95 <= percentage <= 500:
         raise ValueError("Resize percentage must be between -95% and +500%.")
 
+# validate the rotation degrees
 def validate_rotation_degrees(degrees):
     if not -10000 <= degrees <= 10000:
         raise ValueError("Rotation degrees must be between -10000 and +10000.")
 
+
+# Process the image with the sequence of operations,
+# Call the appropriate classes from ImageProcessor.py
 def process_image_sequence(image_bytes, operations):
     print("Reached the ProcessingService.py")
     file_bytes = np.asarray(bytearray(image_bytes), dtype=np.uint8)
@@ -79,8 +89,9 @@ def process_image_sequence(image_bytes, operations):
     _, final_buf = cv2.imencode(f'.{image_format}', image)
     final_image_bytes = io.BytesIO(final_buf).getvalue()
     
-    # Store the final processed image with datetime in this format: final_processed_image_YYYY_MM_DD_HH:MM:SS.{image_format}
+    # Store the final processed image with datetime to differentiate
     final_filename = f"final_processed_image_{datetime.now().strftime('%Y_%m_%d_%H-%M-%S_%f')}.{image_format}"
     processed_images.insert(0, (final_filename, final_image_bytes))  # Ensure the final image is the first in the list
 
+    # Return the list of processed images
     return processed_images
